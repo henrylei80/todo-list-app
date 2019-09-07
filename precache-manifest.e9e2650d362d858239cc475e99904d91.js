25bd78f10051872f77ef575934858c19dec9f39e
Hong <henrylei80@yahoo.com>
2019-09-07 13:06:28 -0400

    Updates

---
+++ precache-manifest.e9e2650d362d858239cc475e99904d91.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10402f395dfb9859d15adf5483900fb",
+    "url": "/todo-list-app/index.html"
+  },
+  {
+    "revision": "7947a2485b0153361f65",
+    "url": "/todo-list-app/static/css/main.1c78c23e.chunk.css"
+  },
+  {
+    "revision": "d934349b72c7ad75bcac",
+    "url": "/todo-list-app/static/js/2.5d894587.chunk.js"
+  },
+  {
+    "revision": "7947a2485b0153361f65",
+    "url": "/todo-list-app/static/js/main.e88c2760.chunk.js"
+  },
+  {
+    "revision": "b0f1992e4453e5e0945a",
+    "url": "/todo-list-app/static/js/runtime~main.4a52f3f0.js"
+  }
+]);
